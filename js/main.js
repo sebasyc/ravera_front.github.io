43d1ed06f8e9da1b6d229a88f0fe5de7ec8da255
Sebastian Campos <pseba@outlook.com>
2020-11-23 18:20:47 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready( function () {
+    $('.cont-slide').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 900,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    })
+})
