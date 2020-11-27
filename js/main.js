7a5463856860996b8959a7c0bde5152491032bb6
Sebastian Campos <pseba@outlook.com>
2020-11-27 11:33:23 -0300

    ultimos ajustes del home Ravera

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready( function () {
 
+    $('.btn-menu-xs').click( function () {
+        $(this).toggleClass('open')
+        $('body').toggleClass('fixed')
+        $('.header-nav').toggleClass('open')
+    })
+
     $('.cont-slide').slick({
         infinite: true,
         slidesToShow: 1,
@@ -22,7 +28,84 @@ $(document).ready( function () {
         autoplay: true,
         autoplaySpeed: 5000,
         pauseOnHover: false,
-        pauseOnFocus: false
+        pauseOnFocus: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    })
+
+    $('.products-slide').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplay: true,
+        autoplaySpeed: 11000,
+        arrows: false,
+        dots: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    })
+
+    $('.slide-marcas').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        swipe: false,
+        arrows: false,
+        speed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
     })
 
 })
